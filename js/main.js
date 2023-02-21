7120d3f11b6f2f39f56958266377c7c8fe0f93ee
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-21 16:39:26 +0100

    devo sistemare i decimali

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@
 
 //chiedo all' utente quanti km deve percorrere con un prompt trovando cosi' la variabile 1
 
-const kmUtente= Number(prompt('Quanti km deve percorrere?'));
+const kmUtente= parseInt(prompt('Quanti km deve percorrere?'));
 
 //chiedo all' utente quanti anni ha con un prompt trovando cosi' la variabile 2
 
-const etaUtente= Number(prompt('Quanti anni ha?'));
+const etaUtente= parseInt(prompt('Quanti anni ha?'));
 
 //ottengo la costante del prezzo base moltiplicando i km per il prezzo/km
 
-let prezzoBaseTragitto =`('kmUtente' * 0,21)`;
+let prezzoBaseTragitto = kmUtente * 0.21;
 console.log(prezzoBaseTragitto);
+
+//ora, a seconda dell'eta' forniro' il prezzo con la scontistica giusta se presente o quello base con sole due cifre decimali
+
+if (etaUtente < 18) {
+    console.log(`per te al prezzo agevolato di ${prezzoBaseTragitto * 0.8}`)
+} else if (etaUtente > 65) {
+    console.log(`per lei al prezzo agevolato di ${prezzoBaseTragitto * 0.6}`)
+} else {
+    console.log(`per lei alla tariffa standard di ${prezzoBaseTragitto}`)
+}
+
