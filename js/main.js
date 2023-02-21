337ebba6ab8ce027aa4d4fa7e59cd8379ac234b5
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-21 15:21:58 +0100

    da finire

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //chiedo all' utente quanti km deve percorrere con un prompt trovando cosi' la variabile 1
 
 const kmUtente= Number(prompt('Quanti km deve percorrere?'));
@@ -10,5 +11,5 @@ const etaUtente= Number(prompt('Quanti anni ha?'));
 
 //ottengo la costante del prezzo base moltiplicando i km per il prezzo/km
 
-let prezzoBaseTragitto =`(${kmUtente} * 0,21)`;
+let prezzoBaseTragitto =`('kmUtente' * 0,21)`;
 console.log(prezzoBaseTragitto);
