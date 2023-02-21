2e8615d9355a5cf9d3f2f9503c7672de3d640634
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-21 15:15:02 +0100

    guardando come si fanno operazioni

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+'use strict';
+
 //chiedo all' utente quanti km deve percorrere con un prompt trovando cosi' la variabile 1
+
+const kmUtente= Number(prompt('Quanti km deve percorrere?'));
+
+//chiedo all' utente quanti anni ha con un prompt trovando cosi' la variabile 2
+
+const etaUtente= Number(prompt('Quanti anni ha?'));
+
+//ottengo la costante del prezzo base moltiplicando i km per il prezzo/km
+
+let prezzoBaseTragitto =`(${kmUtente} * 0,21)`;
+console.log(prezzoBaseTragitto);
